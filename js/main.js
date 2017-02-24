3a1088f51b85263496ee7f949a8e7919ad42e689
Ilijevski <ilijevski93@gmail.com>
2017-02-24 19:49:30 +0100

    ONE MORE NO MORE

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// COUNTDOWN
-$('#countdown').countdown('2018/10/10', function(event) {
-  var $this = $(this).html(event.strftime(''
-//+ '<li><span>%w</span> weeks</li> '
-    + '<li><span>%D</span> days</li> '
-    + '<li><span>%H</span> hr</li> '
-    + '<li><span>%M</span> min</li> '
-    + '<li><span>%S</span> sec</li>'));
-});
-
-
 // initialize and configuration for wow js - animations
 wow = new WOW({
     animateClass: 'animated',
@@ -26,42 +15,6 @@ $('[data-toggle="tooltip"]').tooltip();
 $('[data-toggle=popover]').popover();
 })
 
-// js counters
-$('#about-counter').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-    if (visible) {
-        $(this).find('.timer').each(function() {
-            var $this = $(this);
-            $({
-                Counter: 0
-            }).animate({
-                Counter: $this.text()
-            }, {
-                duration: 2000,
-                easing: 'swing',
-                step: function() {
-                    $this.text(Math.ceil(this.Counter));
-                }
-            });
-        });
-        $(this).unbind('inview');
-    }
-});
-
-// cart widget toggle
-$(function() {
-    $("#items-counter").click(function() {
-        $("body").toggleClass("cart-widget-open");
-    });
-    $("#cart-widget-close").click(function() {
-        $("body").toggleClass("cart-widget-open");
-    });
-
-        $(".cart-widget-close-overlay").click(function() {
-        $("body").toggleClass("cart-widget-open");
-    });
-
-});
-
 //initialize swipers
 //home slider
 var swiper = new Swiper('.home-slider', {
@@ -69,72 +22,6 @@ var swiper = new Swiper('.home-slider', {
     paginationClickable: true,
     nextButton: '.home-slider-next',
     prevButton: '.home-slider-prev'
-});
-
-//testimonials slider
-var swiper = new Swiper('.testimonials-slider', {
-    pagination: '.testimonials-pagination',
-    paginationClickable: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    nextButton: '.testimonials-slider-next',
-    prevButton: '.testimonials-slider-prev'
-});
-
-// product list
-var swiper = new Swiper('.product-list-slider', {
-    slidesPerView: 3,
-    pagination: '.product-list-pagination',
-    paginationClickable: true,
-    nextButton: '.product-list-slider-next',
-    prevButton: '.product-list-slider-prev',
-    spaceBetween: 30,
-    breakpoints: {
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        420: {
-            slidesPerView: 1,
-            spaceBetween: 10
-        }
-    }
-});
-
-//post slider
-var swiper = new Swiper('.post-slider', {
-    pagination: '.post-pagination',
-    paginationClickable: true,
-    nextButton: '.post-slider-next',
-    prevButton: '.post-slider-prev',
-    slidesPerView: 3,
-    spaceBetween: 30,
-    breakpoints: {
-        1024: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        },
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        }
-    }
 });
 
 // smoth scroll
